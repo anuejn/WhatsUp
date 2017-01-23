160935fc0fde6e15165346517ecf1072da0b7bb1
jaro <jarohabiger@googleamil.com>
2017-01-23 18:34:37 +0100

    fix wordmerge

--- code/frontend/js/word_merge.js
+++ code/frontend/js/word_merge.js
@@ -5,10 +5,11 @@
 function word_merge(list) {
     modList = list.slice();
     modList.forEach((nowItem, nowCount, nowObject) => {
-        var regex = new RegExp("^" + nowItem["_id"].toLowerCase() + '.{0,2}','g');
+        var regex = new RegExp("^" + nowItem["_id"] + '.{0,2}','g');
         nowObject.forEach((item, index, object) => {
-            if(nowItem["_id"].toLowerCase() == item["_id"].toLowerCase()) return;
-            checkWord = item["_id"].toLowerCase();
+            if(nowItem["_id"] == item["_id"]) return;
+            if(nowItem["_id"].length < 4) return;
+            checkWord = item["_id"];
             if(checkWord.match(regex)) {
                 nowObject[nowCount]["value"] += item["value"];
                 object.splice(index, 1);
