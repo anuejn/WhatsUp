24ea62a62e393758fa6965f3d9b56a619597ea33
jaro <jarohabiger@googleamil.com>
2017-01-23 13:31:38 +0100

    cleanup

--- code/frontend/js/function_inline.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jaro on 30.12.16.
- * TODO:
- * This file should contain a method, that inlines the data into a function, so it can be run remotely on an other machine
- * It might be used, to declare map/reduce database requests comfortable in your frontend code.
- *
- * The difficultie is to parse all the js and inline rcoursively
- */
